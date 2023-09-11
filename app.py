b7fdb71763c33b575eedec60bf7e14e90afaafb5
Shivangisahu0110 <shivangi.sahu0110@gmail.com>
2023-09-12 00:32:21 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request,jsonify
+from flask_cors import CORS,cross_origin
+import requests
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen as uReq
+import logging
+import pymongo
+logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+import os
+
+app = Flask(__name__)
+
+@app.route("/", methods = ['GET'])
+def homepage():
+    return render_template("index.html")
+
+@app.route("/review" , methods = ['POST' , 'GET'])
+def index():
+    if request.method == 'POST':
+                try:
+
+                    # query to search for images
+                    query = request.form['content'].replace(" ","")
+
+                            # directory to store downloaded images
+                    img_dir = "images/"
+                    if not os.path.exists(img_dir):
+                        os.makedirs(img_dir)
+
+                            # fake user agent to avoid getting blocked by Google
+                    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"}
+
+                            # fetch the search results page
+                    response = requests.get(f"https://www.google.com/search?q={query}&sxsrf=AJOqlzUuff1RXi2mm8I_OqOwT9VjfIDL7w:1676996143273&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiq-qK7gaf9AhXUgVYBHYReAfYQ_AUoA3oECAEQBQ&biw=1920&bih=937&dpr=1#imgrc=1th7VhSesfMJ4M")
+
+
+                            # parse the HTML using BeautifulSoup
+                    soup = bs(response.content, "html.parser")
+
+                            # find all img tags
+                    image_tags = soup.find_all("img")
+
+                            # download each image and save it to the specified directory
+                    del image_tags[0]
+                    mongo_img=[]
+                    for i in image_tags:
+                                # get the image source URL
+                                image_url = i['src']
+                                                       
+                                # send a request to the image URL and save the image
+                                image_data = requests.get(image_url).content
+                                mydict={"Index":image_url,"Image":image_data}
+                                mongo_img.append(mydict)
+                                with open(os.path.join(img_dir, f"{query}_{image_tags.index(i)}.jpg"), "wb") as f:
+                                    f.write(image_data)
+
+                    from pymongo.mongo_client import MongoClient
+                    uri = "mongodb+srv://shisahu:shivangi@cluster0.bejov5u.mongodb.net/?retryWrites=true&w=majority"
+                    # Create a new client and connect to the server
+                    client = MongoClient(uri)
+                    # Send a ping to confirm a successful connection
+                    try:
+                        client.admin.command('ping')
+                        print("Pinged your deployment. You successfully connected to MongoDB!")
+                    except Exception as e:
+                        print(e)
+                    db = client["image_db"]
+                    record = db["image_record"]
+                    record.insert_many(mongo_img)
+
+                    return "image laoded"
+                except Exception as e:
+                    logging.info(e)
+                    return 'something is wrong'
+            # return render_template('results.html')
+
+    else:
+        return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
